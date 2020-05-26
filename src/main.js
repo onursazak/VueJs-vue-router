0a609df3db7c84426b2ef71d2da7d657983f9260
Onur <onur.sazak@hotmail.com.tr>
2020-05-26 21:24:51 +0300

    controlling scroll behavior

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
-import { routes }  from './routes';
+import { routes } from './routes';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'  //default is 'hash'
+  mode: 'history',  //default is 'hash'
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      // örnek olarak /user/1 den editUser a tıklandığında sayfanın konumu (x ve y offset) 
+      // nasılsa tekrar bu sayfaya dönüldüğünde o konuma scroll edilir.
+      return savedPosition;
+    }
+    if (to.hash) {
+      debugger;
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
 });
 new Vue({
   el: '#app',
